9d4f3ed8b161c810945744bb62404f0beda7a09e
Andy <andy@indinero.com>
2013-07-30 14:00:56 -0700

    fix for rails 3

--- lib/rack/oauth2/models/client.rb
+++ lib/rack/oauth2/models/client.rb
@@ -17,7 +17,7 @@ module Rack
         validates_uniqueness_of :code
         validates_uniqueness_of :secret
 
-        before_validation_on_create :assign_code_and_secret
+        before_create :assign_code_and_secret
 
         # Create a new client. Client provides the following properties:
         # # :display_name -- Name to show (e.g. UberClient)
